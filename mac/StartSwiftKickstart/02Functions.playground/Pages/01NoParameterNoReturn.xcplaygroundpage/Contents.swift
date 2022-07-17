8fb53b77b9d2420fbf5b36f31cfeba71d36f51d4
Greg <code@gwilson.dev>
2022-07-17 17:12:59 -0500

    Notes

--- mac/StartSwiftKickstart/02Functions.playground/Pages/01NoParameterNoReturn.xcplaygroundpage/Contents.swift
+++ mac/StartSwiftKickstart/02Functions.playground/Pages/01NoParameterNoReturn.xcplaygroundpage/Contents.swift
@@ -2,7 +2,8 @@
 //: [TOC](00TOC) | Previous | [Next](@next)
 
 func hello() {
-    
+    print("Hello World!")
 }
 
+hello()
 //: [TOC](00TOC) | Previous | [Next](@next)
--- mac/StartSwiftKickstart/02Functions.playground/Pages/02ReturnValues.xcplaygroundpage/Contents.swift
+++ mac/StartSwiftKickstart/02Functions.playground/Pages/02ReturnValues.xcplaygroundpage/Contents.swift
@@ -1,6 +1,10 @@
 //: ### Return Values
 //: [TOC](00TOC) | [Previous](@previous) | [Next](@next)
 
+func hello() -> String {
+    return "Hello, World!"
+}
 
+hello()
 
 //: [TOC](00TOC) | [Previous](@previous) | [Next](@next)
